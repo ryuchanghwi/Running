9178c6bde2345ee86ecb022a1f05798a9c6b30a7
ryuchanghwi <78063938+ryuchanghwi@users.noreply.github.com>
2025-06-05 16:41:33 +0900

    feat: 프로젝트 세팅

---
+++ Running/Running/RunningApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningApp.swift
+//  Running
+//
+//  Created by 류창휘 on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RunningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
